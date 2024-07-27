a253c1ca5a4b2660690fe31d0fceb0275cb27748
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2024-07-27 11:19:14 +0100

    update productshowcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-[linear-gradient(#ffffff,#D2DCFF_100%)]">
+    <section className="bg-gradient-to-b from-[#ffffff] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container flex flex-col items-center">
-        <div className="flex flex-col items-center pt-24 md:max-w-[540px]">
+        <div className="flex flex-col items-center md:max-w-[540px]">
           <div className="px-3 py-1 rounded-lg border border-[#222222]/10 inline-flex items-center justify-center font-medium text-sm w-fit">
             Boost your productivity
           </div>
@@ -15,29 +15,31 @@ export const ProductShowcase = () => {
             A more effective way to track progress
           </h2>
           <p className="text-center text-pretty text-[22px] text-[#010D3E]">
-            Celebrate the joy of accomplishment with an app designed to track
-            your progress and motivate your efforts.
+            Effortlessly turn your ideas into a fully functional, responsive
+            SaaS website in just minutes with this template.
           </p>
         </div>
-        <Image
-          src={productImage}
-          className="pt-10"
-          alt={"Image of the product"}
-        ></Image>
-        <Image
-          src={pyramidImage}
-          className="hidden md:block absolute bottom-[340px] -right-24"
-          alt={"Pyramid Image"}
-          width={220}
-          height={220}
-        ></Image>
-        <Image
-          src={tubeImage}
-          className="hidden md:block absolute bottom-20 -left-24"
-          alt={"Tube Image"}
-          width={220}
-          height={220}
-        ></Image>
+        <div className="relative">
+          <Image
+            src={productImage}
+            className="pt-10"
+            alt={"Image of the product"}
+          ></Image>
+          <Image
+            src={pyramidImage}
+            className="hidden md:block absolute -top-24 -right-32"
+            alt={"Pyramid Image"}
+            width={262}
+            height={262}
+          ></Image>
+          <Image
+            src={tubeImage}
+            className="hidden md:block absolute bottom-8 lg:bottom-24 -left-36 lg:-left-40"
+            alt={"Tube Image"}
+            width={262}
+            height={262}
+          ></Image>
+        </div>
       </div>
     </section>
   );
