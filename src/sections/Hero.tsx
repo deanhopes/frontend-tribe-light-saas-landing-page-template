bfda1dc04676d6f658a4457c97e1b069e552ed39
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2024-07-27 19:19:29 +0100

    hero animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,31 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import cyclinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
 
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  useEffect(() => {
+    setShouldAnimate(true);
+  }, []);
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip"
+    >
       <div className="container">
         <div className=" md:flex items-center">
           <div className="md:w-[478px]">
@@ -26,24 +45,42 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="Cog image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-5, 5],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut",
+              }}
             />
-            <Image
-              src={cyclinderImage}
+            <motion.img
+              src={cyclinderImage.src}
               alt="Cylinder image"
               className="hidden md:block -top-8 -left-32 lg:-left-20 md:absolute"
               width={220}
               height={220}
+              initial={{ y: 0 }}
+              style={{
+                translateY: shouldAnimate ? translateY : 0,
+              }}
             />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               alt="Noodle image"
-              className="hidden lg:block top-[524px] left-[448px] md:absolute rotate-[30deg]"
+              className="hidden lg:block top-[524px] left-[448px] md:absolute"
               width={220}
               height={220}
+              initial={{ y: 0, rotate: 30 }}
+              style={{
+                translateY: shouldAnimate ? translateY : 0,
+                rotate: 30,
+              }}
             />
           </div>
         </div>
