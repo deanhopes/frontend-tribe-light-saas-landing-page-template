3c28b047cc12ab01214cd9aeaae4455edf28bc20
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2024-07-28 15:27:10 +0100

    added testimonials animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -10,6 +12,10 @@ import avatar9 from "@/assets/avatar-9.png";
 
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+
+import React from "react";
+
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -75,31 +81,47 @@ const thirdColumn = testimonials.slice(6, 9);
 const TestimonialsColumn = (props: {
   className?: string;
   testimonials: typeof testimonials;
+  duration?: number;
 }) => (
-  <div className={twMerge("mt-10 flex flex-col gap-6", props.className)}>
-    {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div
-        key={username}
-        className="border p-10 border-[#F1F1F1] rounded-3xl shadow-[0_8px_16px_#00000010] max-w-xs w-full"
-      >
-        <div>{text}</div>
-        <div className="flex gap-3 pt-5 items-center">
-          <Image
-            src={imageSrc}
-            width={40}
-            height={40}
-            alt={`${name}'s Profile Image`}
-            className="size-10"
-          />
-          <div>
-            <p className="font-bold tracking-tight leading-5">{name}</p>
-            <p className="tracking-tight leading-5  text-black/50">
-              {username}
-            </p>
-          </div>
-        </div>
-      </div>
-    ))}
+  <div className={props.className}>
+    <motion.div
+      animate={{ translateY: "-50%" }}
+      transition={{
+        repeat: Infinity,
+        ease: "linear",
+        repeatType: "loop",
+        duration: props.duration || 10,
+      }}
+      className="mt-10 flex flex-col gap-6 pb-6"
+    >
+      {[...new Array(2)].fill(0).map((_, index) => (
+        <React.Fragment key={index}>
+          {props.testimonials.map(({ text, imageSrc, name, username }) => (
+            <div
+              key={username}
+              className="border p-10 border-[#F1F1F1] rounded-3xl shadow-[0_8px_16px_#00000010] max-w-xs w-full"
+            >
+              <div>{text}</div>
+              <div className="flex gap-3 pt-5 items-center">
+                <Image
+                  src={imageSrc}
+                  width={40}
+                  height={40}
+                  alt={`${name}'s Profile Image`}
+                  className="size-10"
+                />
+                <div>
+                  <p className="font-bold tracking-tight leading-5">{name}</p>
+                  <p className="tracking-tight leading-5  text-black/50">
+                    {username}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </React.Fragment>
+      ))}
+    </motion.div>
   </div>
 );
 
@@ -115,15 +137,17 @@ export const Testimonials = () => {
             essential tool for users around the world.
           </p>
         </div>
-        <div className="flex items-center justify-center gap-6 [mask-image:linear-gradient(to_top,transparent,black,black,transparent)]">
-          <TestimonialsColumn testimonials={firstColumn} />
+        <div className="flex items-start justify-center gap-6 [mask-image:linear-gradient(to_top,transparent,black,black,transparent)] max-h-[738px] overflow-hidden">
+          <TestimonialsColumn testimonials={firstColumn} duration={25} />
           <TestimonialsColumn
             testimonials={secondColumn}
             className="hidden md:block"
+            duration={20}
           />
           <TestimonialsColumn
             testimonials={thirdColumn}
             className="hidden lg:block"
+            duration={25}
           />
         </div>
       </div>
